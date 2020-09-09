ae5e32b9a4e8e7f2da9ab2bbcf12d3de30cb09a6
YeonGyeongLee <37134357+yeony22@users.noreply.github.com>
2020-09-09 19:06:12 +0900

    jdbc 수정

--- src/com/common/JDBCTemplate.java
+++ src/com/common/JDBCTemplate.java
@@ -6,70 +6,83 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.mysql.cj.protocol.Resultset;
+
 public class JDBCTemplate {
 	public static Connection getConnection() {
-		Connection conn = null;
-	
-	
-	try {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-	} catch (ClassNotFoundException e) {
-		System.out.println("ClassNotFoundException:" + e.getMessage());
-		System.out.println("드라이버 로딩 실패");
-	} 
-	
-	try {
-		String url="jdbc:mysql://localhost:3306/practice3?characterEncoding=UTF-8&serverTimezone=UTC&useSSL=false";
-		conn = DriverManager.getConnection(url, "root", "Weak");
-		conn.setAutoCommit(false);
-	} catch (SQLException e) {
-		System.out.println("SQL 예외  : " + e.getMessage());
-		System.out.println("SQL state : " + e.getSQLState());
-		System.out.println("vendor 에러 : " + e.getErrorCode());
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		} catch(ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException : " + e.getMessage());
+		
+		}
+		try {
+			String url = "jdbc:mysql://localhost:3306/practice3?characterEndoding=UTF-8&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+			con = DriverManager.getConnection(url, "root", "1234");
+			con.setAutoCommit(false);
+			
+		} catch(SQLException e) {
+			System.out.println("SQLException : " + e.getMessage());
+		
+		}
+		return con;
 	}
-	return conn;
-}
 	
 	public static void close(Connection con) {
 		try {
 			if(con != null && !con.isClosed())
 				con.close();
+			
 		} catch(SQLException e) {
 			e.printStackTrace();
+		
 		}
 	}
 	
 	public static void close(Statement stmt) {
 		try {
-			if(stmt != null && !stmt.isClosed())
+			if(stmt != null & !stmt.isClosed())
 				stmt.close();
-		} catch (SQLException e) {
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
+			
 		}
 	}
 	
 	public static void close(ResultSet rset) {
 		try {
-			if(rset != null && !rset.isClosed()) 
+			if(rset != null && !rset.isClosed())
 				rset.close();
-		} catch (SQLException e) {
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
+		
 		}
 	}
-	public static void commit (Connection con) {
+	
+	public static void commit(Connection con) {
 		try {
-			if (con != null && !con.isClosed())
+			if(con != null && !con.isClosed())
 				con.commit();
-		} catch (SQLException e) {
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
+			
 		}
 	}
+
 	public static void rollback(Connection con) {
 		try {
-			if (con != null && !con.isClosed())
+			if(con != null && !con.isClosed())
 				con.rollback();
-		} catch (SQLException e) {
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
+			
 		}
 	}
 }
